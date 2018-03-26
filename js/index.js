6d08065f9523c8f19c25d51b81e615e23378afa0
Chiamaka Ikeanyi <chiamakaikeanyi@gmail.com>
2018-03-26 18:51:12 +0100

    Added Math functions

--- js/index.js
+++ js/index.js
@@ -21,17 +21,79 @@ var outputField = document.getElementById('outputfield');
          
         var clicked_btn = obj.innerHTML;
          
-        if (clicked_btn == '=') {
-            // Calculate
-            outputField.innerHTML = eval(outputField.innerHTML);
-             
-        } else if (clicked_btn == '1/x') {
-            // reciprocal
-            outputField.innerHTML = 1/outputField.innerHTML;
-        } else if (clicked_btn == 'AC' || clicked_btn == 'DEL') {
-            // clear screen
-            outputField.innerHTML = '0';
-        } else {
+            if (clicked_btn == '=') {
+                // Calculate
+                outputField.innerHTML = eval(outputField.innerHTML);       
+            }
+            else if (clicked_btn == '1/x') {
+                // reciprocal
+                outputField.innerHTML = 1/outputField.innerHTML;
+            } else if (clicked_btn == '√') {
+                // squareroot
+                outputField.innerHTML = Math.sqrt(outputField.innerHTML);  
+            } else if (clicked_btn == 'x^2') {
+                // square
+                outputField.innerHTML *= outputField.innerHTML;  
+            }else if (clicked_btn == 'x^3') {
+                // square
+                outputField.innerHTML *= outputField.innerHTML * outputField.innerHTML;  
+            } else if (clicked_btn == 'Rnd') {
+                // exponential
+                outputField.innerHTML = Math.round(outputField.innerHTML);
+            } else if (clicked_btn == 'Rnd') {
+                // pi
+                outputField.innerHTML = Math.PI(outputField.innerHTML);
+            } else if (clicked_btn == 'asin') {
+                // arcsine
+                outputField.innerHTML = Math.asin(outputField.innerHTML);
+            }
+            else if (clicked_btn == 'acos') {
+                // arccosine
+                outputField.innerHTML = Math.acos(outputField.innerHTML);
+            }
+            else if (clicked_btn == 'atan') {
+                // arctan
+                outputField.innerHTML = Math.atan(outputField.innerHTML);
+            }
+            else if (clicked_btn == 'ln2') {
+                // arccosine
+                outputField.innerHTML = Math.LN2;
+            }
+            else if (clicked_btn == 'E') {
+                // base of natural logarithms
+                outputField.innerHTML = Math.E;
+            }
+            else if (clicked_btn == 'sin') {
+                // sine
+                outputField.innerHTML = Math.sin(outputField.innerHTML);
+            }
+            else if (clicked_btn == 'cos') {
+                // cosine
+                outputField.innerHTML = Math.cos(outputField.innerHTML);
+            }
+            else if (clicked_btn == 'tan') {
+                // tan
+                outputField.innerHTML = Math.tan(outputField.innerHTML);
+            }
+      
+            else if (clicked_btn == 'log') {
+                // logarithm
+                outputField.innerHTML = Math.log(outputField.innerHTML);
+            }
+            else if (clicked_btn == '%') {
+                // squareroot
+                outputField.innerHTML = outputField.innerHTML / 100;  
+            } 
+      
+            else if (clicked_btn == 'AC' || clicked_btn == 'DEL') {
+                // clear screen
+                outputField.innerHTML = '0';
+            } else if (clicked_btn == 'EXP') {
+                // exponential
+                outputField.innerHTML = Math.exp(outputField.innerHTML);
+            }
+            
+          else {
             if (outputField.innerHTML == '0') {
                 outputField.innerHTML = clicked_btn;
                  
